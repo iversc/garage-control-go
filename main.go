51e83b2fea3bfe73dc4213fd7d9d45dca853bbf7
Chris Iverson <cj.no.one@gmail.com>
2024-02-26 20:26:47 -0600

    Most commands complete, hue request in progress.

--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"errors"
-	"net/http"
-	"os"
-	"github.com/gorilla/mux"
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha1"
+	"crypto/tls"
 	"encoding/hex"
-	"time"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
 	"strings"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 var authCode []byte 
-
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("got / request")
-	io.WriteString(w, "Success\n")
-}
+var hueUser string
 
 func checkAuth(clientAuthCode string) bool {
 	cacBytes, err := hex.DecodeString(clientAuthCode)
@@ -54,22 +54,100 @@ func checkAuth(clientAuthCode string) bool {
 	return false
 }
 
+func deactivate() {
+    time.Sleep(1 * time.Second)
+    cmd := exec.Command("gpio", "-g", "write", "4", "0")
+    if err := cmd.Run(); err != nil {
+        fmt.Println("gpio off call failed.")
+        return
+    }
+
+    fmt.Println("Button reset.")
+}
+
+func activate() {
+    cmd := exec.Command("gpio", "-g", "write", "4", "1")
+    if err := cmd.Run(); err != nil {
+        fmt.Println("gpio on call failed.")
+        return
+    }
+
+    go deactivate()
+
+    fmt.Println("Button triggered.")
+}
+
+func shutdown(sdType string) {
+    if sdType == "-h" {
+        fmt.Println("Shutting down Pi.")
+    } else if sdType == "-r" {
+        fmt.Println("Rebooting Pi.") 
+    } else {
+        fmt.Printf("Unknown shutdown type %s.\n",sdType)
+        return
+    }
+
+    cmd := exec.Command("shutdown", sdType, "now")
+    if err := cmd.Run(); err != nil {
+        fmt.Println("shutdown call failed.")
+        return
+    }
+}
+
+func switchLights(enabled string) {
+    body := `{ "on": {"on": ` + enabled + "}}"
+    reqURL := "https://philips-hue/clip/v2/resource/grouped_light/b6faa6e6-dba5-4e11-81e1-bf92f67b9280"
+    bodyReader := bytes.NewReader([]byte(body))
+
+    req, err := http.NewRequest(http.MethodPut, reqURL, bodyReader)
+    if err != nil {
+        fmt.Printf("error making http request: %s\n", err)
+        os.Exit(1)
+    }
+
+    req.Header.Set("hue-application-key", hueUser)
+    req.Header.Set("Content-Type", "application/json")
+
+    customTransport := http.DefaultTransport.(*http.Transport).Clone()
+    customTransport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+    client := &http.Client{Transport: customTransport}
+
+    res, err := client.Do(req)
+    if err != nil {
+        fmt.Printf("Error making request: %s\n", err)
+        os.Exit(1)
+    }
+
+    resBody, err := io.ReadAll(res.Body)
+    if err != nil {
+        fmt.Printf("Unable to read response: %s\n", err)
+        os.Exit(1)
+    }
+
+    fmt.Printf("Response: %s\n", resBody)
+}
+
 func runCommand(cmd string, w http.ResponseWriter) {
 	var msg string
 	if cmd == "activate" {
 		fmt.Println("Door activate command received.")
+        activate()
 		msg = "Door Activated"
 	} else if cmd == "shutdown" {
 		fmt.Println("Shutdown command received.")
+        shutdown("-h")
 		msg = "Shutting down"
 	} else if cmd == "reboot" {
 		fmt.Println("Reboot command received.")
+        shutdown("-r")
 		msg = "Rebooting..."
 	} else if cmd == "lightson" {
 		fmt.Println("Lights on command received.")
+        switchLights("true")
 		msg = "Lights On"
 	} else if cmd == "lightsoff" {
 		fmt.Println("Lights off command received.")
+        switchLights("false")
 		msg = "Lights Off"
 	} else {
 		fmt.Printf("Unknown command '%s' received.\n", cmd)
